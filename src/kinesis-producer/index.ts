8b5f9c7b316ce522cbf1f8f91db71433a16a371c
Ben Main <bmain@lumeris.com>
2019-04-29 18:39:15 -0500

    Ready to publish.

---
+++ src/kinesis-producer/index.ts
@@ -0,0 +1,4 @@
+export * from './batch-kinesis-publisher';
+export * from './retrying-batch-kinesis-publisher';
+export * from './kinesis-event.interface';
+export * from './kinesis-producer.module';
