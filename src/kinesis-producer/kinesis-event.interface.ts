5a8ed29161ce721559185ecd6aedbeb3ec882767
Victor Frank <vfrank@lumeris.com>
2020-11-04 11:38:38 -0700

    correct the use of a class in the readme

--- src/kinesis-producer/kinesis-event.interface.ts
+++ src/kinesis-producer/kinesis-event.interface.ts
@@ -2,10 +2,3 @@ export interface KinesisEvent {
   PartitionKey: string;
   Data: string;
 }
-
-export class KinesisEvent {
-  constructor(partitionKey: string, data: string) {
-    this.PartitionKey = partitionKey;
-    this.Data = data;
-  }
-}
